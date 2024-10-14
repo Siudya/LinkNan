617bb672e771e19fa65bb3e5e414540fff89832d
cyril0124 <211998078@qq.com>
2024-10-14 11:19:53 +0800

    chore: add `xmake.lua` and `README.md`

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nansha
+`Nansha` is the top-level of the [ZhuJiang](https://github.com/Siudya/ZhuJiang) RingBus-based NoC system which integrates the [Nanhu](https://github.com/Siudya/Nanhu) high performance RISC-V core and ZhuJiang interconnect.
+
+# Compile source code
+Notice: Make sure that you have installed `xmake`. If not, please refer to the [xmake](https://github.com/xmake-io/xmake) official website for installation.
+```bash
+xmake run init
+xmake run comp
+```
---
+++ xmake.lua
@@ -0,0 +1,31 @@
+local prj_dir = os.getenv("PWD")
+
+target("idea")
+    set_kind("phony")
+
+    -- xmake run idea
+    on_run(function (target)
+        os.exec("mill -i mill.idea.GenIdea/idea")
+    end)
+
+target("init")
+    set_kind("phony")
+
+    -- xmake run init
+    on_run(function (target)
+        local exec = os.exec
+        exec("git submodule update --init")
+        os.cd(prj_dir .. "/dependencies/nanhu")
+        exec("git submodule update --init coupledL2 huancun")
+        os.cd(prj_dir .. "/dependencies/nanhu/coupledL2")
+        exec("git submodule update --init AXItoTL")
+    end)
+
+target("comp")
+    set_kind("phony")
+
+    -- xmake run comp
+    on_run(function (target)
+        os.exec("mill -i nansha.compile")
+        os.exec("mill -i nansha.test.compile")
+    end)
